f9d90595ada43fcd9459a5cb63707bf40e5c8cf0
KALrious <j.gautier180293@gmail.com>
2023-11-07 13:38:48 +0100

    feat add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SuperHerosModule } from './super-heros/super-heros.module';
 
 @Module({
-  imports: [SuperHerosModule],
+  imports: [ConfigModule.forRoot(), SuperHerosModule],
   controllers: [AppController],
   providers: [AppService],
 })
