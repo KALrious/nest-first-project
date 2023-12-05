37c3169611766b2fb9048413cc25ca7dc65b0f2b
KALrious <j.gautier180293@gmail.com>
2023-12-05 17:46:39 +0100

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,15 +8,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('hello')
-  getCustomHello(): string {
-    return this.appService.getCustomHello();
-  }
-
-  @Post('message')
-  postMessage(@Body() { name }: { name: string }): string {
-    return this.appService.postMessage(name);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,12 +5,4 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-
-  getCustomHello(): string {
-    return 'Hello World from Jean !';
-  }
-
-  postMessage(name: string): string {
-    return `Message posted by ${name} !`;
-  }
 }
