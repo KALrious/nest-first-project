2f67a1c07dab723c41a51d671c2182683520a274
KALrious <j.gautier180293@gmail.com>
2023-12-10 16:26:20 +0100

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('hello/:id')
+  @HttpCode(205)
+  getHello(@Query('toto') toto, @Param('id') id, @Body() body): string {
     return this.appService.getHello();
   }
 }
