20f07965ab688f76126471bed67e64debcbc74a3
KALrious <j.gautier180293@gmail.com>
2024-01-03 16:59:05 +0100

    4-1

--- src/dog/dog.module.ts
+++ src/dog/dog.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { DogController } from './dog.controller';
 import { DogService } from './dog.service';
-
+@Global()
 @Module({
   controllers: [DogController],
   providers: [DogService],
