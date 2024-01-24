b17dab394f94b680eb118c030ee3a9d9587542f9
KALrious <j.gautier180293@gmail.com>
2024-01-24 16:27:31 +0100

    test5-2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BirdModule } from './bird/bird.module';
 import { DogController } from './dog/dog.controller';
 import { DogModule } from './dog/dog.module';
+import { User } from './user/entities/user.entity';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [BirdModule, DogModule],
+  imports: [
+    BirdModule,
+    DogModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController, DogController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
---
+++ src/user/user.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+@Module({
+  controllers: [UserController],
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UserService],
+})
+export class UserModule {}
