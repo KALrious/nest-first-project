8dd10a81604fa6c18bb049e29848eae7bf216dea
KALrious <j.gautier180293@gmail.com>
2024-02-16 13:43:03 +0100

    fix(user): remove not use transaction

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,28 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
-import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
   constructor(private dataSource: DataSource) {}
-
-  async findAll(): Promise<User[]> {
-    const queryRunner = this.dataSource.createQueryRunner()
-
-    await queryRunner.connect()
-    await queryRunner.startTransaction();
-    try {
-        await queryRunner.manager.save({ ... });
-        await queryRunner.manager.save({ user2 });
-
-        // ...
-        await queryRunner.commitTransaction();
-    } catch (err) {
-        await queryRunner.rollbackTransaction();
-    } finally {
-await queryRunner.release()
-    }
-
-    
-  }
 }
