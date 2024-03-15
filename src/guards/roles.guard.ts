59b955e2d4705230ac7ee6e98f25a057fde3b130
KALrious <j.gautier180293@gmail.com>
2024-03-15 14:30:54 +0100

    add guard

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -15,6 +15,6 @@ export class RolesGuard implements CanActivate {
     }
     const request = context.switchToHttp().getRequest();
     const user = request.user;
-    return roles.map((role) => user.include());
+    return roles.some((role) => user.roles.include(role));
   }
 }
